cba85daa30bb43427fcc02575143c27c0a1479dd
MariaKonstantinov <konstantinovamariia1@gmail.com>
2022-11-26 17:59:38 +0200

    add functionality

--- components/SubmitForm.js
+++ components/SubmitForm.js
@@ -4,9 +4,12 @@
 
 // We pass the selector of the form template element to the form class constructor
 
+// Having collected the input fields' data, we need to pass it to the UserCard class in order to return the markup. We agreed to avoid using tight coupling when setting relationships between classes. That's why all operations — such as building cards and embedding the form in markup — that happen when the form is submitted will be passed in the body of a callback function. Let's name this callback handleFormSubmit():
+
 class SubmitForm {
-  constructor({ formSelector }) {
+  constructor({ formSelector, handleFormSubmit }) {
     this._formSelector = formSelector;
+    this._handleFormSubmit = handleFormSubmit;
   }
 
   //   2. copying method for returning card markup - _getTemplate method from Card.js and replacing all "card" instances in elements with "form" instances
@@ -21,13 +24,32 @@ class SubmitForm {
     return formElement;
   }
 
+  // In the _setEventListeners() method, we need to call _handleFormSubmit() method when submitting the form. We also need to pass an object returned by the _getInputValues() function as an argument to it:
+
   _setEventListeners() {
     this._element.addEventListener("submit", (evt) => {
       //   preventing default behavior of browser
       evt.preventDefault();
+
+      // Add a _handleFormSubmit() function call
+      // Pass an object which is the result of the _getInputValues work to it
+      this._handleFormSubmit(this._getInputValues());
+
       //   resetting the form after submission
       this._element.reset();
     });
+  }
+
+  // we create a private method that builds an array from all the form fields, iterates over them and adds each value of the array to an object. The name attribute of each field will serve as a key in this object.
+  _getInputValues() {
+    this._inputList = this._element.querySelectorAll(".form__input");
+
+    this._formValues = {};
+    this._inputList.forEach(
+      (input) => (this._formValues[input.name] = input.value)
+    );
+
+    return this._formValues;
   }
 
   // We need to create a public method in the SubmitForm class for returning the markup for our HTML. We'll name it generateForm()
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ import {
 
 // 2. In our case with Form, we don't need to do it as we just need to add a Form to the DOM.
 
-// create an instance of Section class - a cardList instance
+// create an instance of Section class - a cardList instance ----------------------------------->
 const cardList = new Section(
   {
     data: messageList,
@@ -36,15 +36,28 @@ const cardList = new Section(
 
 cardList.renderItems();
 
-// create an instance of SubmitForm class - a form instance
-const form = new SubmitForm({ formSelector: ".form-template" });
+// create an instance of SubmitForm class - a form instance ------------------------------------>
+// The formData parameter of the function is a value we pass to this._handleFormSubmit() when calling it. In other words, it's an object returned by the _getInputValues() method. The SubmitForm class allows us to create several forms with different fields and an event listener function.
+
+const form = new SubmitForm({
+  formSelector: ".form-template",
+  handleFormSubmit: (formData) => {
+    // we pass the formData object containing data from the form
+    // to a new instance of UserCard class
+    const card = new UserCard(formData, ".card-template_type_user");
+
+    const cardElement = card.generateCard();
+
+    cardList.setItem(cardElement);
+  },
+});
 
 // call generateForm
 const formElement = form.generateForm();
 
 // We use method 2: use Section to place form on DOM, passing it an object (for data it will simply be an array), as we just have a list of messages (messageList) that we need to render to the DOM; we use the container defined to us in constants,js (formSection); and we don't need a custom RENDERER function as all we need is to call -setItems- method, we don't need to pass the initial array to the Section class so we are going to initialize it only to use its -setItem- method
 
-// create an instance of Section class - a formRenderer instance
+// create an instance of Section class - a formRenderer instance ------------------------------->
 const formRenderer = new Section(
   {
     data: [],
@@ -57,4 +70,5 @@ formRenderer.setItem(formElement);
 // Stages:
 // 1. Create a class that stores the form markup: class SubmitForm() in SubmitForm.js;
 // 2. Add this markup to the page;
-// 3. When the form is submitted, create an instance of UserCard and insert it into the markup. Define the relationship between UserCard and SubmitForm.
+// 3. When the form is submitted, create an instance of UserCard and insert it into the markup.
+// 4. Define the relationship between UserCard and SubmitForm.
